166cff90a4c5e1603c48ea85d8c15af20fa5bb68
ahmadsofuwan <sofuwan62@gmail.com>
2024-12-17 22:09:54 +0700

    a

--- index.sh
+++ index.sh
@@ -9,7 +9,7 @@ PASSWORD="Ahmadsofuwan@123"
 databases=`mysql -u $USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "| " | grep -v Database`
 
 for db in $databases; do
-    if [[ "$db" != "information_schema" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
+    if [[ "$db" != "information_schema" ]] && [[ "$db" != "sys" ]] && [[ "$db" != "performance_schema" ]] && [[ "$db" != "mysql" ]] && [[ "$db" != _* ]] ; then
         echo "Dumping database: $db"
         mysqldump -u $USER -p$PASSWORD --databases $db > `date +%Y%m%d`.$db.sql
        # gzip $OUTPUT/`date +%Y%m%d`.$db.sql
